0f9c3ff55d157285cf6ffba17b8bd53fe4a43a14
lychee <linzhihui2022@outlook.com>
2023-07-09 11:13:53 +0800

    feat: setup workflow

--- apps/coco-live/__test__/example.spec.tsx
+++ apps/coco-live/__test__/example.spec.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { CcButton } from "../src/components/atom/CcButton";
+import React from "react";
 
 describe("example", () => {
   it("has a button", async () => {
-    render(<button>button</button>);
+    render(<CcButton label="button" />);
     expect(screen.getByRole("button").textContent).toBe("button");
   });
 });
