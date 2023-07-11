401134c5d33b4712128289cc8fa6b196f7e9e526
lychee <linzhihui2022@outlook.com>
2023-07-11 20:53:49 +0800

    feat: init a webhook

--- apps/coco-live/src/app/api/webhook/route.ts
+++ apps/coco-live/src/app/api/webhook/route.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from 'next/server'
 
 export function GET(request: Request) {
-  return new NextResponse(request.url, {
+  return new NextResponse(`GET: ${request.url}`, {
+    status: 200,
+  })
+}
+
+export function POST(request: Request) {
+  return new NextResponse(`POST: ${request.url}`, {
     status: 200,
   })
 }
