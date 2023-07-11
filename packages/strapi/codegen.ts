7c415d7728ad4010e4c100da01f1360ed6286753
lychee <linzhihui2022@outlook.com>
2023-07-12 00:00:55 +0800

    feat: download schema

--- packages/strapi/codegen.ts
+++ packages/strapi/codegen.ts
@@ -1,33 +1,29 @@
-import type { CodegenConfig } from "@graphql-codegen/cli";
+import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://coco-connect-cms.jan24th.today/graphql",
+  schema: 'http://coco-connect-cms.jan24th.today/graphql',
   // schema: "./schema.graphql",
-  documents: "src/**/*.gql",
+  documents: 'src/**/*.gql',
   generates: {
-    "./src/generated.ts": {
+    './src/generated.ts': {
       plugins: [
-        {
-          add: {
-            content:
-              'import type { DocumentNode } from "graphql/language/ast";',
-          },
-        },
-        {
-          add: {
-            content: "/* eslint-disable */",
-          },
-        },
-        "typescript",
-        "typescript-operations",
-        "typescript-graphql-request",
+        'typescript',
+        'typescript-operations',
+        'typescript-graphql-request',
+      ],
+      config: {
+        scalars: {},
+      },
+    },
+    './src/schema.graphql': {
+      plugins: [
+        'schema-ast',
       ],
       config: {
         scalars: {},
       },
     },
   },
-};
-
-export default config;
+}
+export default config
