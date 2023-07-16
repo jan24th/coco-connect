5354bff1946071b1bf2c6b3c1c8a7ed61c88d815
lychee <linzhihui2022@outlook.com>
2023-07-16 19:20:38 +0800

    feat: fix storybook

--- apps/coco-live/src/components/atom/CcTheme.stories.tsx
+++ apps/coco-live/src/components/atom/CcTheme.stories.tsx
@@ -1,17 +1,21 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import labels from '../../i18n/messages/en.json'
 import { CcTheme, CcThemeProviders } from './CcTheme.client'
+import { CcLocaleProvider } from '@/app/locale.client'
 
 const meta: Meta<typeof CcTheme> = {
   title: 'atom/CcTheme',
   component: () => (
-    <CcThemeProviders>
-      <CcTheme />
-      <div>
-        <span>Hello </span>
-        <span className="text-green-500 dark:text-red-500">world</span>
-      </div>
-    </CcThemeProviders>
+    <CcLocaleProvider locale='en' labels={labels}>
+      <CcThemeProviders>
+        <CcTheme />
+        <div>
+          <span>Hello </span>
+          <span className="text-green-500 dark:text-red-500">world</span>
+        </div>
+      </CcThemeProviders>
+    </CcLocaleProvider>
   ),
 }
 
